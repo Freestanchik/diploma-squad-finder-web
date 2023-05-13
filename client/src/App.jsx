1bb0a3caa2dcc38de41be136e663965a61d2d372
Oleksandr Bihun <bihun.sasha2019@gmail.com>
2023-05-13 17:51:14 +0300

    Add navigation router to client; Add login and registration pages with scss styles;

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,32 @@
 import './App.scss'
 import Footer from "./components/footer/Footer.jsx";
 import Header from "./components/header/Header.jsx";
+import {Routes, Route} from "react-router-dom";
+import Registration from "./pages/registration/Registration.jsx";
+import GameSessions from "./pages/gameSessions/gameSessions.jsx";
+import React from "react";
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Login from "./pages/login/Login.jsx";
 
 function App() {
 
     return (
         <div className={"App"}>
             <Header/>
-            <main>main component</main>
+            <main>
+                <Routes>
+                    <Route path={"/"} element={<GameSessions/>}></Route>
+                    <Route path={"/register"} element={<Registration/>}></Route>
+                    <Route path={"/login"} element={<Login/>}> </Route>
+                </Routes>
+            </main>
             <Footer/>
+            <ToastContainer/>
         </div>
     )
 }
+
+
 
 export default App
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,8 +2,21 @@ import logo from '../../assets/logo.png'
 import './header.scss'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faUser} from '@fortawesome/free-regular-svg-icons';
+import {Link, useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {logout, reset} from "../../store/slices/authSlice.js";
 
 const Header = () => {
+    const {user} = useSelector((state) => state.auth)
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
     return (
         <header className={'header'}>
             <div className={'container'}>
@@ -13,11 +26,18 @@ const Header = () => {
                 </div>
                 <div className={'header__user-info'}>
                     <div className={'user-info_left'}>
-                        <p className={'user-name'}>Oleksandr Bihun</p>
-                        <span className={'login-link'}></span>
+                        <p className={'user-name'}>{user ? user.login : "Гість"}</p>
+                        <span className={'login-link'}>
+                            {user
+                                ? <button className="btn-logout" onClick={onLogout}>Вийти</button>
+                                : <Link to="/login">Увійти</Link>
+                            }
+                        </span>
                     </div>
                     <span className={'profile-icon'}>
-                        <FontAwesomeIcon icon={faUser}/>
+                        <Link to="/login">
+                            <FontAwesomeIcon icon={faUser}/>
+                        </Link>
                     </span>
                 </div>
             </div>
