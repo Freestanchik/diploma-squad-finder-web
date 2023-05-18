b4b99ebbb57d08e6a8fdeb39e099740ae27390da
Oleksandr Bihun <bihun.sasha2019@gmail.com>
2023-05-18 13:53:42 +0300

    Add game sessions and user game sessions pages

--- client/src/pages/gameSessions/gameSessions.jsx
+++ client/src/pages/gameSessions/gameSessions.jsx
@@ -1,8 +1,59 @@
+import React, {useEffect} from "react";
+import GameSessionList from "../../components/gameSessionList/GameSessionList.jsx";
+import GameSessionForm from "../../components/gameSessionForm/GameSessionForm.jsx";
+import "./gameSessions.scss"
+import Modal from "../../components/modal/Modal.jsx";
+import {useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {getAllGameSessions, resetGameSessions} from "../../store/slices/gameSessionSlice.js";
+import Loading from "../../components/loading/Loading.jsx";
+import {faWindowClose} from "@fortawesome/free-regular-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {toast} from "react-toastify";
 
 
 const GameSessions = () => {
-    return(
-        <div>game Sessions</div>
+
+    const [formModalIsOpen, setFormIsOpen] = React.useState(false);
+    const {token} = useSelector((state) => state.auth)
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {gameSessions, isLoading, isSuccess, isError, message} = useSelector((state) => state.gameSession)
+
+    useEffect(() => {
+        dispatch(getAllGameSessions())
+    }, [isError, isSuccess, message, dispatch])
+    if (isLoading) {
+        return <Loading/>
+    }
+
+
+    function openFormModal() {
+        if (!token) {
+            navigate('/login')
+        }
+        setFormIsOpen(true);
+    }
+
+    function closeFormModal() {
+        setFormIsOpen(false);
+    }
+
+    return (
+        <div className={"game-session-page"}>
+            <button className="btn-open-form" onClick={openFormModal}>Створити сесію</button>
+
+            <Modal active={formModalIsOpen} setActive={setFormIsOpen}>
+                <div className="modal-form">
+                    <FontAwesomeIcon icon={faWindowClose} className="btn-close-modal"
+                                     onClick={closeFormModal}>close</FontAwesomeIcon>
+                    <GameSessionForm/>
+                </div>
+            </Modal>
+
+            <GameSessionList gameSessions={gameSessions}/>
+        </div>
     );
 };
 
